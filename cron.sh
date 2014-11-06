0c71bed60d174438d352380bd91b8b93bd13be9d
root <root@hamming.urgu.org>
2014-11-06 13:40:52 +0500

    CRON jobs to create new image and do cleanup

---
+++ cron.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+BASE=$(dirname "$0")
+CONFIG="$BASE/conf/$(hostname -f).sh"
+
+locked() {
+  flock "$BASE" "$@"
+}
+
+silent() {
+  local allfile="$(tempfile -p cow)"
+  "$@" >"$allfile" 2>&1; rv="$?"
+  [[ "$rv" -ne 0 ]] && cat "$allfile"
+  rm "$allfile"
+  return "$rv"
+}
+
+if [[ ! -f "$CONFIG" ]]; then
+  echo "no config file: $CONFIG"
+  exit 1
+fi
+
+case "$1" in
+  new)
+    silent locked "$BASE/update.sh" "$CONFIG"
+  ;;
+  cleanup)
+    silent locked "$BASE/iet.py" "$CONFIG"
+  ;;
+  *)
+    echo "usage: $0 {new,cleanup}"
+    exit 1
+  ;;
+esac
