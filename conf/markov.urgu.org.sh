dc1c615558cf1e78e101e9eab8561deeae6d82e3
root <root@markov.urgu.org>
2014-11-16 02:40:23 +0500

    ready to deploy to production, markov gets config

---
+++ conf/markov.urgu.org.sh
@@ -0,0 +1,24 @@
+# Path to the reference VM configuration file for Xen.
+REF_VM_PATH=/root/xen/image64.cfg
+
+# Path to the test VM config file for Xen.
+TEST_VM_PATH=/root/xen/image64-test.cfg
+
+# Host name of test VM, used for graceful reboot, if possible.
+TEST_HOST=image64-test.urgu.org
+
+# FQDN of target host for iSCSI clients
+ISCSI_TARGET_HOST=m.fb.urgu.org
+
+# Size of snapshot volume used for hosting over iSCSI.
+# Format may be anything accepted by lvcreate.
+SNAPSHOT_SIZE=5G
+
+# Suffix which will be appended to timestamps and reference
+# VM disk snapshots to differ it from another disks. Do not
+# mix with test vm name!
+TIMESTAMP_SUFFIX=at-
+
+# A path where vmlinuz and initrd files will be served by
+# a web server for network booting.
+WEB_PATH=/var/www
