5ac1a613e9f648fb2830088b5ef0c3db3246d500
root <root@hamming.urgu.org>
2015-10-30 01:08:38 +0500

    Add cron entries for hamming, host config for markov.

---
+++ conf/host/markov.urgu.org.sh
@@ -0,0 +1,9 @@
+# Target host for iSCSI daemon and web server.
+TARGET_HOST=m.fb.urgu.org
+
+# Port used to connect to iSCSI daemon.
+ISCSI_TARGET_PORT=3260
+
+# A path where vmlinuz and initrd files will be served by
+# a web server for network booting.
+WEB_PATH=/var/lib/cow
