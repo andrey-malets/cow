1a83c2215807e09b999251fcbd1b59695339da76
root <root@hamming.urgu.org>
2015-11-04 03:32:19 +0500

    New configs.

--- conf/image/image64-beta.urgu.org.sh
+++ conf/image/image64-beta.urgu.org.sh
@@ -1,11 +1,11 @@
 # Path to the reference VM configuration file for Xen.
-REF_VM_PATH=/root/xen/image32.cfg
+REF_VM_PATH=/root/xen/image64.cfg
 
 # Path to the test VM config file for Xen.
-TEST_VM_PATH=/root/xen/image32-test.cfg
+TEST_VM_PATH=/root/xen/image64-test.cfg
 
 # Host name of test VM, used for graceful reboot, if possible.
-TEST_HOST=image32-test.urgu.org
+TEST_HOST=image64-test.urgu.org
 
 # Size of snapshot volume used for hosting over iSCSI.
 # Format may be anything accepted by lvcreate.
@@ -16,26 +16,29 @@ SNAPSHOT_SIZE=5G
 # mix with test vm name!
 TIMESTAMP_SUFFIX=at-
 
-# The names of partitions system will use for booting
+# The names of partitions system will use for creating COW image
+# and booting it.
 declare -A PARTITION_NAMES
 
-PARTITION_NAMES[base]=image32-base
+# The name of partition on the base image which will be used to
+# create COW image.
+PARTITION_NAMES[base]=image64-base
 
-# This is a name of partition which must be present in the system
+# The name of partition which must be present in the system
 # (usually mounted over iSCSI) when network boot is selected
-PARTITION_NAMES[network]=cow-image32-net
+PARTITION_NAMES[network]=cow-image64-net
 
-# This is a partition name which must be present (presumably on
+# The name of partition which must be present (presumably on
 # one of local disks) when local boot is done
-PARTITION_NAMES[local]=cow-image32-local
+PARTITION_NAMES[local]=cow-image64-local
 
-# This is the a name of special partition for copy-on-write device.
+# The name of special partition for copy-on-write device.
 # This should reside on the local disk and be unique for each
 # machine.
-PARTITION_NAMES[cow]=cow-image32-cow
+PARTITION_NAMES[cow]=cow-image64-cow
 
 # This is a name for config partition and it's signature used to
 # verify config partition integrity. Should be present at the same
 # disk as cow partition.
-PARTITION_NAMES[conf]=cow-image32-conf
-PARTITION_NAMES[sign]=cow-image32-sign
+PARTITION_NAMES[conf]=cow-image64-conf
+PARTITION_NAMES[sign]=cow-image64-sign
