7950ff0cbe2625b05a321ac5c8f4c96a36c7f71d
root <root@hamming.urgu.org>
2017-10-09 13:01:58 +0500

    Add keyutils and initramfs-tools as prereqs.

--- tocopy/root/cow/prereqs.sh
+++ tocopy/root/cow/prereqs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -xe
 
-packages=(open-iscsi kpartx puppet dpkg-dev mdadm augeas-tools watchdog)
+packages=(open-iscsi kpartx puppet dpkg-dev mdadm augeas-tools watchdog
+          keyutils initramfs-tools)
 
 PATH="$(dirname "$0")/fake:$PATH" apt-get -y install "${packages[@]}"
