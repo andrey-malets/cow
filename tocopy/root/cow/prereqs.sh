a606e411eab69b99ea8b6f5ed3874dab70e2a2f6
root <root@ritchie.urgu.org>
2022-11-08 01:23:49 +0500

    Remove unnecessary files

--- tocopy/root/cow/prereqs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-packages=(open-iscsi kpartx puppet dpkg-dev mdadm augeas-tools watchdog
-          keyutils initramfs-tools)
-
-PATH="$(dirname "$0")/fake:$PATH" apt-get -y install "${packages[@]}"
