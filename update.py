9e0c9198c8a7c18086167ad405dea4aa369a67d5
root <root@ritchie.urgu.org>
2022-11-18 20:59:31 +0500

    Remove timestamps from log.
    All the runs are supposed to be done via systemd noe,
    which automatically records timstamps for us.

--- update.py
+++ update.py
@@ -1196,8 +1196,7 @@ def configure_logging(args):
     }
     logging.basicConfig(
         level=levels.get(args.verbose, logging.DEBUG),
-        format='%(asctime)s: %(levelname)-8s ' '%(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        format='%(levelname)-8s ' '%(message)s',
     )
 
 
