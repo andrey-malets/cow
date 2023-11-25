5f11a03d231faea7db79c0bfa5fe639536f8615c
root <root@ritchie.urgu.org>
2023-11-25 11:59:04 +0500

    use more robust method to detect if iSCSI session has transient/permanent error.

--- tocopy/root/cow/watchdog.sh
+++ tocopy/root/cow/watchdog.sh
@@ -4,7 +4,16 @@ if ! grep -q 'cowsrc=network' /proc/cmdline; then
     exit 0
 fi
 
-if ! dmesg | grep -q 'detected conn error'; then
+if dmesg | grep -q 'session recovery timed out'; then
+    dmesg -T | mail -s "FAILURE: iSCSI session did not recover!" root
+    sleep 3
+    exit 1
+elif iscsiadm -m session -P 3 | grep -q 'iSCSI Session State: FAILED'; then
+    (iscsiadm -m session -P 3; dmesg -T) | mail -s "WARNING: iSCSI session failure" root
+    sleep 3
+    exit 0
+else
+    # dmesg -T | mail -s "I'm alive" root
     exit 0
 fi
 
