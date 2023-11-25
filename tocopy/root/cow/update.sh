51c5a72deb1f8e11c06b9630a83e26876c38beab
root <root@ritchie.urgu.org>
2023-11-25 11:58:02 +0500

    clear puppet caches during image cleanup, properly handle absent mdadm.

--- tocopy/root/cow/update.sh
+++ tocopy/root/cow/update.sh
@@ -2,9 +2,10 @@
 
 set -x -e
 
-# disable puppet and remove it's monitoring file
+# disable puppet, remove SSL info, caches and monitoring file
 puppet agent --disable
 rm /var/lib/puppet/ssl/{certs,public_keys,private_keys}/*
+rm -rf /var/cache/puppet/*
 rm -f /usr/local/puppet.random
 
 # clear transient home
@@ -13,15 +14,17 @@ rm -rf /home/*
 # clear logs
 find /var/log -type f -delete
 
-# clear MDADM array information
-echo -n > /etc/mdadm/mdadm.conf
+if [ -d /etc/mdadm ]; then
+    # clear MDADM array information
+    echo -n > /etc/mdadm/mdadm.conf
+
+    # tell MDADM to assemble all of its arrays in initramfs
+    augtool -s set /files/etc/default/mdadm/INITRDSTART all
+fi
 
 # clear udev net and net generator rules
 rm -f /etc/udev/rules.d/*net.rules \
       /lib/udev/rules.d/*net-generator.rules
-
-# tell MDADM to assemble all of its arrays in initramfs
-augtool -s set /files/etc/default/mdadm/INITRDSTART all
 
 # clear host SSH keys
 # TODO: replace host key with temporary key
